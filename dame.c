106559d41e9721f972a671e5a0a7e72f5f1d0e7f
Daniela Catana <daniela.catana@student.upt.ro>
2022-12-16 23:28:04 +0200

    Adaugare repo si initiere fisier

---
+++ dame.c
@@ -0,0 +1,15 @@
+
+//Problema care rezolva asezarea a "n" dame(regine) pe tabla de sah
+#include <stdio.h>
+
+//variabila in care salvam numarul damelor
+int nr_dame;
+
+int main() {
+    
+    printf("Introduceti numarul de dame:");
+    scanf("%d",&nr_dame);
+    printf("Numarul dame introdus: %d",nr_dame);
+    
+    return 0;
+}
