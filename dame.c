4606c7d69d0d31d2d18b99c93bd2941051335bbc
Daniela Catana <daniela.catana@student.upt.ro>
2022-12-17 23:15:07 +0200

    Adaugare functie pentru afisare dame

--- dame.c
+++ dame.c
@@ -4,6 +4,24 @@
 
 //variabila in care salvam numarul damelor
 int nr_dame;
+int dame[8];
+int varianta=1;
+
+void afisare_Regine(){
+    printf("Varianta "+varianta);
+    varianta++;
+    for(int i=0; i<8; i++) {
+        for (int j=0; j<8; j++) {
+            if (dame[i] == j) {
+                printf("%d ",dame[i]);
+            }
+            else {
+                printf("* ");
+            }
+        }
+    }
+    
+}
 
 int main() {
     
