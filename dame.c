46f044d2367ee4dbdd01c311c88e65e042295cef
Daniela Catana <daniela.catana@student.upt.ro>
2022-12-19 10:31:21 +0200

    Adaugare comentarii si indentare cod

--- dame.c
+++ dame.c
@@ -2,12 +2,16 @@
 //Problema care rezolva asezarea a "n" dame(regine) pe tabla de sah
 #include <stdio.h>
 
-//constanta unde definim numarul de dame. Daca se doreste un alt numar, se schimba valoarea
-#define NR_DAME 4
+//constanta unde definim numarul de dame. Daca se doreste un alt numar, se schimba valoarea aici
+#define NR_DAME 8
 
+//vector unde salvam damele
 int dame[NR_DAME];
+//variabila unde contorizam numarul de variante gasite
 int varianta=1;
 
+/*in acesta functie se afiseaza asezarea damelor pe tabla de saj
+se va marga in "matricea de sah" cate o dama cu numarul aferent ei si restul pozitiilor cu asterix*/
 void afisare_Regine(){
     printf("Varianta %d\n",varianta);
     varianta = varianta + 1;
@@ -26,7 +30,10 @@ void afisare_Regine(){
     
 }
 
+/*Functie backtraking unde se cauta toate solutiile de asezarea a damelor
+Se genereaza toate permutarile posibile folosind backtraking*/
 void back(int k){
+    //conditia de oprire a backtrakingului este cand am generat solutii pentru toate damele
     if(k == NR_DAME) {
         afisare_Regine();
     }
@@ -40,22 +47,27 @@ void back(int k){
     }
 }
 
+//Functie in care verificam sa nu avem conflict intre dame
+//Se stie ca 2 dame nu au voie sa fie pe aceeasi linie, coloana sau diagonala
 int verificare_Consistenta(int nr){
     for(int i=0; i<nr; i++) {
-        if(dame[i] == dame[nr])
+        if(dame[i] == dame[nr]) //verificam daca avem regina pe aceeasi coloana
             return 0;
-        if ((dame[i] - dame[nr]) == (nr - i)) 
+        if ((dame[i] - dame[nr]) == (nr - i)) // verificam daca avem regina pe aceeasi diagonala principala
             return 0;
-        if ((dame[nr] - dame[i]) == (nr - i))
+        if ((dame[nr] - dame[i]) == (nr - i)) // verificam daca avem regina pe aceeasi diagonala secundara
             return 0;
     }
     return 1;
 }
 
+/*Functia principala de unde incepe executia programului.
+Se apeleaza functia de backtrking (incepem de la pozitia 0) pentru generarea tuturor solutiilor*/
 int main() {
     
-   varianta=1;
-   back(0);
+    back(0);
+    
+    printf("Pentru a aseza %d dame pe tabla de sah exista %d variante.",NR_DAME,varianta-1);
     
     return 0;
 }
